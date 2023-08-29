1bd73debe6571e33b72e2a86df4fc372b9e45733
Vignesh Murugan <vigmurug@gmail.com>
2023-08-29 11:05:30 +0530

    Create About.js

---
+++ Ep07-Finding-the-path/Coding/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <h2>created successfully!</h2>
+    </div>
+  )
+}
+
+export default About
