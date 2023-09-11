0875177c2d202ad8e714c2d5f84668652062d2f7
Vignesh Murugan <vigmurug@gmail.com>
2023-09-11 18:57:39 +0530

    Create constants.js

---
+++ Ep08-Lets-get-classy/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
+export const LOGO_URL = "https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg?w=2000";
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complelte-menu=true&lat=12.970882&lng=77.500366&restaurantId="
